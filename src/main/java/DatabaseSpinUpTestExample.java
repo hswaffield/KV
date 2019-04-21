38855ad1b9fec7e3299ea66b86d5fbb0af37ae5e
Henry Swaffield <henry.swaffield@liveramp.com>
2019-04-21 14:37:17 -0700

    simple example

---
+++ src/main/java/DatabaseSpinUpTestExample.java
@@ -0,0 +1,47 @@
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class DatabaseSpinUpTestExample {
+
+  public static void main(String[] args) throws Exception {
+
+    // a static method that loads the RocksDB C++ library.
+    RocksDB.loadLibrary();
+
+    // the Options class contains a set of configurable DB options
+    // that determines the behaviour of the database.
+    try (final Options options = new Options().setCreateIfMissing(true)) {
+
+      // a factory method that returns a RocksDB instance
+      try (final RocksDB db = RocksDB.open(options, "/Users/hswaff/code/KV/db")) {
+
+        byte[] hi_key = "hi_key".getBytes();
+        byte[] hi_value = "hi_value".getBytes();
+
+        byte[] hi_key_2 = "hi_key_2".getBytes(StandardCharsets.UTF_8);
+        byte[] hi_value_2 = "hi_value_2".getBytes(StandardCharsets.UTF_8);
+
+        // do something
+        db.put(hi_key, hi_value);
+        db.put(hi_key_2, hi_value_2);
+
+        System.out.println(Arrays.toString(db.get(hi_key)));
+        System.out.println(new String(db.get(hi_key), StandardCharsets.UTF_8));
+
+        System.out.println(Arrays.toString(db.get(hi_key_2)));
+        System.out.println(new String(db.get(hi_key_2), StandardCharsets.UTF_8));
+
+        System.out.println("sleeping now");
+        Thread.sleep(45 * 1000);
+        System.out.println("done sleeping now");
+      }
+    } catch (RocksDBException e) {
+      // do some error handling
+      throw e;
+    }
+  }
+}
