295a81fc14171ec4dd75e95d89e2d81504a79e5f
Josh Curtis <jcurtis@liveramp.com>
2019-04-21 16:56:16 -0700

    Make handler static so getDeclaredConstructor() works will find a no argument constructor

--- src/main/java/com/kv/server/Server.java
+++ src/main/java/com/kv/server/Server.java
@@ -23,7 +23,7 @@ public class Server {
     }
   }
 
-  public class KvHandler extends HttpServlet {
+  public static class KvHandler extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
       JSONObject body = new JSONObject();
